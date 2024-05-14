ba4e00bf0fa7c6af1bcd476352c846e8014d4a8f
JilaniRj <138582820+JilaniRj@users.noreply.github.com>
2024-05-14 12:55:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,18 @@
-import unittest
-from equipment import Equipment, MinePlan
+# equipment.py
 
-class TestEquipment(unittest.TestCase):
-    def test_equipment_initialization(self):
-        equipment = Equipment("Excavator", "MineA", "Coal", "2023-05-01", "8 hours", "100 gal diesel", "90%")
-        self.assertEqual(equipment.equipment, "Excavator")
-        self.assertEqual(equipment.mine, "MineA")
-        self.assertEqual(equipment.material, "Coal")
-        self.assertEqual(equipment.prod_date, "2023-05-01")
-        self.assertEqual(equipment.work_hours, "8 hours")
-        self.assertEqual(equipment.fuel_consumed, "100 gal diesel")
-        self.assertEqual(equipment.availability, "90%")
+class Equipment:
+    def __init__(self, equipment, mine, material, prod_date, work_hours, fuel_consumed, availability):
+        self.equipment = equipment
+        self.mine = mine
+        self.material = material
+        self.prod_date = prod_date
+        self.work_hours = work_hours
+        self.fuel_consumed = fuel_consumed
+        self.availability = availability
 
-class TestMinePlan(unittest.TestCase):
-    def test_mine_plan_initialization(self):
-        mine_plan = MinePlan()
-        self.assertIsInstance(mine_plan.equipment_list, list)
+class MinePlan:
+    def __init__(self):
+        self.equipment_list = []
 
-    def test_add_equipment(self):
-        mine_plan = MinePlan()
-        equipment = Equipment("Excavator", "MineA", "Coal", "2023-05-01", "8 hours", "100 gal diesel", "90%")
-        mine_plan.add_equipment(equipment)
-        self.assertEqual(len(mine_plan.equipment_list), 1)
-        self.assertEqual(mine_plan.equipment_list[0], equipment)
-
-if __name__ == '__main__':
-    unittest.main()
+    def add_equipment(self, equipment):
+        self.equipment_list.append(equipment)
